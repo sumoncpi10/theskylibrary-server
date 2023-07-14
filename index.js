40a6050a5a7be14b6b0e11299cae26e554609f9c
sumon <sumoncpi10@gmail.com>
2023-07-15 00:20:41 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,125 @@
+require('dotenv').config();
+const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pyvsvhc.mongodb.net/?retryWrites=true&w=majority`
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    const db = client.db('theskylibrary');
+    const bookCollection = db.collection('books');
+
+    app.get('/products', async (req, res) => {
+      const cursor = bookCollection.find({});
+      const product = await cursor.toArray();
+
+      res.send({ status: true, data: product });
+    });
+
+    app.post('/product', async (req, res) => {
+      const product = req.body;
+
+      const result = await bookCollection.insertOne(product);
+
+      res.send(result);
+    });
+
+    app.get('/product/:id', async (req, res) => {
+      const id = Number(req.params.id);
+      // const id = req.params.id;
+      const result = await bookCollection.findOne({ _id:(id) });
+      console.log(result);
+      res.send(result);
+    });
+
+    app.delete('/product/:id', async (req, res) => {
+      const id = req.params.id;
+
+      const result = await bookCollection.deleteOne({ _id: ObjectId(id) });
+      console.log(result);
+      res.send(result);
+    });
+
+    app.post('/comment/:id', async (req, res) => {
+      const productId = Number(req.params.id);
+      const comment = req.body.comment;
+
+      console.log(productId);
+      console.log(comment);
+
+      const result = await bookCollection.updateOne(
+        { _id: (productId) },
+        { $push: { comments: comment } }
+      );
+
+      console.log(result);
+
+      if (result.modifiedCount !== 1) {
+        console.error('Book not found or comment not added');
+        res.json({ error: 'Book not found or comment not added' });
+        return;
+      }
+
+      console.log('Comment added successfully');
+      res.json({ message: 'Comment added successfully' });
+    });
+
+    app.get('/comment/:id', async (req, res) => {
+      const productId = Number(req.params.id);
+
+      const result = await bookCollection.findOne(
+        { _id: (productId) },
+        { projection: { _id: 0, comments: 1 } }
+      );
+
+      if (result) {
+        res.json(result);
+      } else {
+        res.status(404).json({ error: 'Product not found' });
+      }
+    });
+
+    app.post('/user', async (req, res) => {
+      const user = req.body;
+
+      const result = await userCollection.insertOne(user);
+
+      res.send(result);
+    });
+
+    app.get('/user/:email', async (req, res) => {
+      const email = req.params.email;
+
+      const result = await userCollection.findOne({ email });
+
+      if (result?.email) {
+        return res.send({ status: true, data: result });
+      }
+
+      res.send({ status: false });
+    });
+  } finally {
+  }
+};
+
+run().catch((err) => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send('Hello from The Sjy Library!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
